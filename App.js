bd12231ad067ef0b0b0e6fab9a2278f4c06de2b8
SCP-code <159820723+SCP-code@users.noreply.github.com>
2024-05-03 21:37:16 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh', background: '#fff' }}>
+      <h1 style={{ color: '#4a4a4a', fontSize: '2rem' }}>GenM Studios</h1>
+      <div style={{ marginTop: '2rem', width: '60%', height: '2px', background: '#000' }}></div>
+    </div>
+  );
+};
+
+export default App;
